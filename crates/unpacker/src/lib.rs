a2b31fce2cdda6a6b56ba9c1ca04b5c121728ac1
Harmeet Singh Bhatia <h@thedrone7.dev>
2026-03-09 18:45:12 -0400

    feat: scan dirs to unpack

--- crates/unpacker/src/lib.rs
+++ crates/unpacker/src/lib.rs
@@ -1,7 +1,23 @@
 use anyhow::Result;
+use std::fs;
+use std::path::Path;
 
 pub fn unpack(input: &str, output: &str) -> Result<()> {
     println!("Unpacking from {:?} to {:?}", input, output);
+    scan_dir(Path::new(input))?;
+
     Ok(())
 }
 
+fn scan_dir(dir: &Path) -> Result<()> {
+    for entry in fs::read_dir(dir)? {
+        let path = entry?.path();
+        if path.is_dir() {
+            scan_dir(&path)?;
+        } else if path.extension().and_then(|e| e.to_str()) == Some("ab") {
+            println!("Found: {}", path.display());
+        }
+    }
+
+    Ok(())
+}
